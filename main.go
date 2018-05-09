abf0cf908b5b9a6d1f7bae12fc37445c35771de0
nazul <yanwu401909@gmail.com>
2018-05-09 11:14:46 +0800

    Add sayHi func

--- main.go
+++ main.go
@@ -6,10 +6,20 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1>Welcome to my site!!!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my super awesome site!!!</h1>")
 }
+
+func handlerSayHi(w http.ResponseWriter, r *http.Request){
+	name := r.FormValue("name")
+	if name == ""{
+		name = "lvye"
+	}
+	fmt.Fprint(w, "你好," + name + "!!!")
+}
+
 func main(){
 	log.Println("Server run at port:8000")
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/sayHi", handlerSayHi)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }

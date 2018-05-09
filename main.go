e97a1603ce2cdd63ff76d0f75471eaf8f653e592
nazul <yanwu401909@gmail.com>
2018-05-09 10:33:21 +0800

    Add first func

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 import (
 	"fmt"
+	"net/http"
+	"log"
 )
+
+func handlerFunc(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "<h1>Welcome to my site!!!</h1>")
+}
 func main(){
-	fmt.Println("Hello world!!!!!!!!!!!")
+	log.Println("Server run at port:8000")
+	http.HandleFunc("/", handlerFunc)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
